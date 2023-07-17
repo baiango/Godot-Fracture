d20b2a22e4cfb66a151d970266baa9893312e34c
baiango <105705580+baiango@users.noreply.github.com>
2023-07-17 22:03:43 +0800

    Cp2: Moving into GDScript for faster prototyping
    I switched from image to Raylib for realtime prototyping. But I wondered, why not use Python instead? So I don't had to fighting with static typing. But why use Python when GDScript run faster and it already include the both 2D and 3D graphics engine. So that's why I use GDScript to prototype it.

--- rust_ref/src/types.rs
+++ rust_ref/src/types.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+// #[inline] is forbidden. Just use LTO!
 
 /// # Example
 /// ```
@@ -21,14 +22,35 @@ impl RNG
 {	pub fn new() -> RNG { RNG(1023) }
 	pub fn set(mut self, num: u64) -> Self { self.0 = num; self }
 	pub fn lehmer(mut self) -> Self { self.0 = u64::wrapping_mul(self.0, 0xd1342543de82ef95); self }
+	// LOL! Don't try to use bitwise AND "& 255" or modulo "% 256" on this RNG!
 	pub fn get_u8(self) -> u8 { (self.0 >> (u64::BITS - 8)) as u8 }
 	pub fn get_u16(self) -> u16 { (self.0 >> (u64::BITS - 16)) as u16 }
-	pub fn get_u32(self) -> u32 { (self.0 >> (u64::BITS - 32)) as u32 } }
+	pub fn get_u32(self) -> u32 { (self.0 >> (u64::BITS - 32)) as u32 }}
 
-pub struct Vec2i { w: i32, h: i32 }
+#[derive(Debug)]
+pub struct Vec2i { pub x: i32, pub y: i32 }
 impl Vec2i
-{	pub fn new(w: i32, h: i32) -> Vec2i { Vec2i { w: w, h: h }} }
+{	pub fn new(x: i32, y: i32) -> Vec2i { Vec2i{ x, y } }
+	pub fn expand_1d_to_2d(i: i32, row: i32) -> Vec2i { Vec2i{ x: i % row, y: i / row } }
+	pub fn flat_2d_to_1d(vec: Vec2i, row: i32) -> i32 { vec.x + (vec.y * row) }}
 
-pub struct Vec2u { w: u32, h: u32 }
+pub struct Vec2u { pub x: u32, pub y: u32 }
 impl Vec2u
-{	pub fn new(w: u32, h: u32) -> Vec2u { Vec2u { w: w, h: h }} }
+{	pub fn new(x: u32, y: u32) -> Vec2u { Vec2u{ x, y } } }
+
+pub struct Vec3i { pub x: i32, pub y: i32, pub z: i32 }
+impl Vec3i
+{	pub fn new(x: i32, y: i32, z: i32) -> Vec3i { Vec3i{ x, y, z } }
+	pub fn expand_1d_to_3d(i: i32, row: i32, col: i32) -> Vec3i { Vec3i{ x: i % row, y: i / row, z: i / (row * col) } }
+	pub fn flat_3d_to_1d(vec: Vec3i, row: i32, col: i32) -> i32 { vec.x + (vec.y * row) + (vec.z * row * col) }}
+
+
+/// So, Vec is a pointer located in stack that points to the heap.
+/// And Vec with Vec is a pointer to pointer to the heap.
+/// It's all over the place. We don't want that.
+/// So if you want to give each 1D Vec a different size. Then don't use this.
+/// Else, use this.
+pub struct Vec2d<T> { pub vec: Vec<T>, row: usize, col: usize }
+impl Vec2d<u8>
+{	pub fn new(row: usize, col: usize) -> Vec2d<u8> { Vec2d { vec: vec![0; row * col], row, col } }
+	pub fn get_index(&self, x: usize, y: usize) -> u8 { self.vec[x + (y * self.row)] }}
--- rust_ref/src/voronoi.rs
+++ rust_ref/src/voronoi.rs
@@ -1,26 +1,41 @@
 use crate::*;
-
+use raylib::{prelude::*, ffi::DrawPixel};
 
 pub fn voronoi_test()
-{	let mut img = image::ImageBuffer::new(512, 512);
-	let grid_size = 10;
-	let pixels_per_cell = Vec2u::new(img.width(), img.height());
+{	let mut img = Vec2d::new(512, 512);
+	let mut tmp = vec![vec![Color::BLACK; 512]; 512];
 
-
-	let mut tmp = image::ImageBuffer::new(512, 512);
 	let mut rng = RNG::new();
 	println!("{}", rng.0);
-	for (x, y, pixel) in tmp.enumerate_pixels_mut()
-	{	let r = (0.3 * x as f32) as u8;
-		let g = (0.3 * y as f32) as u8;
+	for (i, arr) in tmp.iter_mut().enumerate()
+	{	
+		// println!("{:?} {:?}", i, arr[i]);
+	}
+	// println!("{:?}", tmp);
 
-		rng = rng.lehmer();
-		let b = rng.clone().get_u8();
-		*pixel = image::Rgb([r, g, b]); }
+	for i in 0..img.vec.len()
+	{	print!("{:?} ", img.get_index(i % 512, i / 512)); }
+	println!();
 
+	let (mut rl, thread) = raylib::init()
+		.size(512, 512)
+		.title("Hello, World")
+		.build();
 
-	for (x, y, pixel) in img.enumerate_pixels_mut()
-	{	*pixel = tmp[(x, y)]; }
+	while !rl.window_should_close() {
+		let mut d = rl.begin_drawing(&thread);
 
-	let err_code = img.save("voronoi.png");
-	println!("{:?}", err_code); }
+		d.clear_background(Color::WHITE);
+
+		// draw the image
+		unsafe {
+		for (i, arr) in tmp.iter().enumerate()
+		{	let pos = Vec2i::expand_1d_to_2d(i as i32, 512);
+			DrawPixel(pos.x, pos.y, Color::BLACK.into());
+			// println!("{:?}", pos);
+		}
+		}
+
+		d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+	}
+}
